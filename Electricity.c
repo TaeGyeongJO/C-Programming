d65213c5185aa33d2a472536108e7779e7429dcc
TaeGyeongJo <jtging8282@gmail.com>
2024-06-20 08:15:18 +0300

    Week6 Electricity bill

--- Electricity.c
+++ Electricity.c
@@ -1,45 +1,38 @@
-// Electricity bill
-# include <stdio.h>
+//calculation of amount to pay in tokens
+#include <stdio.h>
 int main() {
-    int customerID, Unitconsumed;
-    char customerName;
-    float chargeperunit, totalbill, surcharge = 0;
-    printf("Enter the Customer ID: ");
-    scanf("%d", &customerID);
-    
-    printf("Enter CoustomerName:");
-    scanf("%s", customerName);
-    
-    printf("Enter Unit Consumed:");
-    scanf("%d", &Unitconsumed);
-    
-    if (Unitconsumed <= 199) {
-        chargeperunit = 1.20;
-    } else if (Unitconsumed < 400) {
-        chargeperunit = 1.50;
-    } else if (Unitconsumed <600) {
-        chargeperunit = 1.80;
-    } else {
-        chargeperunit = 2.00;
+    int customer_id, unit_consumed;
+    float total_pay, charges;
+    char customer_name[50];
+//propts the user to enter data asked
+    printf("Enter customer ID: ");
+    scanf("%d", &customer_id);
+
+    printf("Enter customer name: ");
+    scanf("%s", customer_name);
+
+    printf("Enter units consumed:");
+    scanf("%d", &unit_consumed);
+//offers the conditions to be met
+    if (unit_consumed <= 199) {
+        charges = 1.20;
+    } else if (unit_consumed >= 200 && unit_consumed < 400) {
+        charges = 1.50;
+    } else if (unit_consumed >= 400 && unit_consumed < 600) {
+        charges = 1.80;
+    } else if (unit_consumed >= 600) {
+        charges = 2.00;
     }
-    
-    totalbill = Unitconsumed * chargeperunit;
-    
-    if (totalbill > 400) {
-        surcharge = totalbill * 0.15;
-        totalbill += surcharge;
-    }
-    
-    if (totalbill < 400) {
-        totalbill = 100;
-    }
-    
-    printf("\nCustomer ID: %d\n", customerID);
-    printf("Customer Name: %s\n", customerName);
-    printf("Unit Consumed: %d\n", Unitconsumed);
-    printf("Total Bill: Ksh %.2f\n", totalbill);
-    
-    
+    //calculates the payments
+     total_pay = unit_consumed * charges;
+if(total_pay>400){
+    total_pay= total_pay+(0.15*total_pay);
+}
+ //resulting output 
+    printf("Customer ID: %d\n", customer_id);
+    printf("Customer Name: %s\n", customer_name);
+    printf("Units Consumed: %d\n", unit_consumed);
+    printf("Charges per unit:ksh %.2f\n", charges);
+    printf("Total pay: %.2f\n", total_pay);
     return 0;
-    
 }
